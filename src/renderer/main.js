31b91865ab299ffd4c7a468fd5cb60b011d8624c
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2018-02-01 16:26:12 +0100

    Imported Semantic UI
    Loaded dependency and use it in Vue app

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
 import axios from 'axios'
 
+import SuiVue from 'semantic-ui-vue'
+import 'semantic-ui-css/semantic.min.css'
+
 import App from './App'
 import router from './router'
 import store from './store'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+
 Vue.http = Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
+
+Vue.use(SuiVue)
 
 /* eslint-disable no-new */
 new Vue({
