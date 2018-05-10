9d54cbda08afaed455df89bc056c1a4ee02064b7
BERTRAND Guillaume <guillaume5524@hotmail.fr>
2018-05-10 21:44:52 +0200

    try:EditorView: Add a simple delete method (1/2)

--- src/renderer/components/EditorView.vue
+++ src/renderer/components/EditorView.vue
@@ -1,5 +1,5 @@
 <template>
-    <body @contextmenu.prevent="$refs.ctxMenu.open">
+    <body @contextmenu.prevent="$refs.ctxMenu.open" @click="affectClickedElement($event)">
         <toolbar></toolbar>
         <visual-engine></visual-engine>
         <context-menu id="context-menu" ref="ctxMenu">
@@ -10,18 +10,27 @@
                 <button id="add-child" class="ui button">
                     Add a child element
                 </button>
-                <button id="delete" class="ui button">
+                <button @click="onDelete()" id="delete" class="ui button">
                     Delete
                 </button>
             </div>
         </context-menu>
     </body>
 </template>
-
 <script>
-  import contextMenu from 'vue-context-menu'
-  export default {
-    name: 'editor',
-    components: { contextMenu }
-  }
+    import contextMenu from 'vue-context-menu'
+export default {
+      name: 'editor',
+      components: { contextMenu },
+      methods: {
+        affectClickedElement (event) {
+          this.$clickedNode = event.target
+        },
+        onDelete () {
+          console.log('we have to get the children node whereas as we are able to get the parent node')
+          console.log('deleted element')
+          console.log(this.$clickedNode)
+        }
+      }
+}
 </script>
